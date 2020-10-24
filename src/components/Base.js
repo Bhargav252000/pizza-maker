7b63042701d000874ee0d3d2d363585ee435fc56
Bhargav Gohil <bhargavgohil25@gmail.com>
2020-10-24 18:07:13 +0530

    types of transitions

--- src/components/Base.js
+++ src/components/Base.js
@@ -6,7 +6,11 @@ const Base = ({ addBase, pizza }) =>{
     const bases = ['Classic', 'Thin & Crispy', 'Thick Crust', 'Cheese Burst Crust' ];
 
     return(
-        <div className = "base container">
+        <motion.div className = "base container"
+            initial= {{x: '100vw'}}
+            animate= {{x: 0}}
+            transition = {{type:"spring", delay: 0.5}}
+        >
             <h3>
                 Step 1: Choose Your Base
             </h3>
@@ -27,13 +31,14 @@ const Base = ({ addBase, pizza }) =>{
                         <motion.button
                             initial={{ x:"-100vw"}}
                             animate={{ x: 0}}
+                            transition={{type:'spring', stiffness: 120}}
                         >
                             Next
                         </motion.button>
                     </Link>
                 </div>
             )}
-        </div>
+        </motion.div>
     ) 
 }
 
