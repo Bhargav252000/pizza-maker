08c742d33a5d8c9b047b60c7603be8cf8b430a5a
Bhargav Gohil <bhargavgohil25@gmail.com>
2020-10-26 17:24:57 +0530

    Updates

---
+++ precache-manifest.026f1bc254eb652085a6c6e7c2c53948.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d04a34dfae7c64b44a67674912c81e9a",
+    "url": "/pizza-maker/index.html"
+  },
+  {
+    "revision": "3ee45a05081aeaa642c4",
+    "url": "/pizza-maker/static/css/main.c31bae75.chunk.css"
+  },
+  {
+    "revision": "2e501566ffeaeac9f35e",
+    "url": "/pizza-maker/static/js/2.2dfd8692.chunk.js"
+  },
+  {
+    "revision": "8da57ccee1690fd1d0790b5fcb8f1bad",
+    "url": "/pizza-maker/static/js/2.2dfd8692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee45a05081aeaa642c4",
+    "url": "/pizza-maker/static/js/main.2fde5c24.chunk.js"
+  },
+  {
+    "revision": "27aa30778230c9207d9b",
+    "url": "/pizza-maker/static/js/runtime-main.b590379a.js"
+  }
+]);
